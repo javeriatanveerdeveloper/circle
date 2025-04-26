7017f092ba4a8721d8f6893468c520f3d28e7402
javeriatanveerdeveloper <alihussainuf456@gmail.com>
2025-04-26 11:25:26 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+let radius=prompt('enter the radius of circule');
+radius=parseInt(radius);
+console.log('RADIUS = ' +radius);
+let diameter= 2 * radius;
+console.log('DIAMETER = ' +diameter);
+const PI=3.1412;
+let circumference= 2 * PI * radius;
+console.log('CIRCUMFERENCE = ' +circumference);
+let area= PI * radius ** 2;
+console.log('AREA = ' +area);
+
+
+
